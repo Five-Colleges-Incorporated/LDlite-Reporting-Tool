6d87cf65e0a4907fe6a84fd07a442e5776286e5e
ameliasutton <ameliasutton@umass.edu>
2026-08-03 11:33:00 -0400

    Use pathlib instead of os. Preserve stack trace for FileNotFound error.

--- adapters/script_files.py
+++ adapters/script_files.py
@@ -1,24 +1,19 @@
-import os
+from pathlib import Path
 
 class ScriptFiles:
     def __init__(self, scripts_directory: str):
-        self.scripts_directory = scripts_directory
+        self.scripts_directory = Path(scripts_directory)
 
     def list_script_files(self) -> list[str]:
-        files = []
-        for file in os.listdir(self.scripts_directory):
-            if file.endswith('.sql'):
-                files.append(file)
+        files = [x.name for x in sorted(self.scripts_directory.glob('*.sql'))]
         return files
 
     def read_script_file(self, script_name: str) -> str:
-        try:
-            with open(f"{os.getenv('query_filepath')}/{script_name}", "r") as q:
-                query = ""
-                for line in q:
-                    query += line
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Query File:\n{script_name}\nnot found")
-        return query
+        scriptPath = self.scripts_directory / script_name
+        try: 
+            script = scriptPath.read_text()
+        except FileNotFoundError as err: 
+            raise FileNotFoundError(f"Query File: {script_name} not found") from err
+        return script
     
     
