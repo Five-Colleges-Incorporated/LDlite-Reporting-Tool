6a98c5277b8cfe4b95de51d45f702c73b2c610dd
Amelia Sutton <ameliasutton@umass.edu>
2026-09-16 08:30:53 -0400

    Create 'handlers' module to handle user interface actions

--- adapters/output_file.py
+++ adapters/output_file.py
@@ -3,15 +3,21 @@ from pathlib import Path
 from collections.abc import Iterator, Buffer
 
 class OutputFile:
-    def __init__(self, out_file_name: str, out_file_directory: str): 
-        self.out_file_name = Path(out_file_directory) / out_file_name
+    def __init__(self, out_file_directory: str): 
+        self.out_file_directory = Path(out_file_directory)
 
     @abstractmethod
-    def write(self, rows: memoryview) -> None: 
+    def write(self, out_file_name: str, rows: memoryview) -> None: 
         pass
 
 class LocalOutputFile(OutputFile): 
+<<<<<<< HEAD
     def write(self, rows:Iterator[Buffer]) -> None:
         with open(self.out_file_name, 'wb') as outfile:
+=======
+    def write(self, out_file_name: str,  rows:memoryview) -> None:
+        out_file_path = self.out_file_directory / out_file_name
+        with open(out_file_path, 'wb') as outfile:
+>>>>>>> 0604eb8 (Create 'handlers' module to handle user interface actions)
             for row in rows:
                 outfile.write(row)
